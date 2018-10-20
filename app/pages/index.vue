d5a620b482b45886a0e11e96091fd95643537cbd
yudedako <pluto.club@gmail.com>
2018-10-20 23:22:27 +0900

    ユーザーインターフェイスの構築

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,66 +1,46 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt-beginners-guide-sample-2
-      </h1>
-      <h2 class="subtitle">
-        My magnificent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <span>ログイン</span>
       </div>
-    </div>
+      <form>
+        <div class="form-content">
+          <span>ユーザー ID</span>
+          <el-input placeholder="" v-model="formData.id" />
+        </div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateMode">アカウントを作成する</el-checkbox>
+        </div>
+        <div class="form-right">
+          <el-button type="primary">{{ buttonText }}</el-button>
+        </div>
+      </form>]
+    </el-card>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: ''
+      }
+    }
+  },
+
+  computed: {
+    buttonText() {
+      return this.isCreateMode ? '新規登録' : 'ログイン'
+    }
   }
 }
 </script>
 
 <style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.form-content {
+  margin: 16px 0;
 }
 </style>
