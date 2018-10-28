98151f04d2879eccb2b3098dcca76bd48a2cadff
yudedako <pluto.club@gmail.com>
2018-10-28 12:12:34 +0900

    no-ssrによるキャッシュ機能の追加

--- app/pages/posts/_id.vue
+++ app/pages/posts/_id.vue
@@ -7,16 +7,18 @@
           <small>by {{ post.user.id }}</small>
         </div>
         <p>{{ post.body }}</p>
-        <p class="text-right">
-          <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked" @click="unlike" round>
-            <span class="el-icon-star-on" />
-            <span>{{ post.likes.length }}</span>
-          </el-button>
-          <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
-            <span class="el-icon-star-off" />
-            <span>{{ post.likes.length }}</span>
-          </el-button>
-        </p>
+        <no-ssr>
+          <p class="text-right">
+            <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked" @click="unlike" round>
+              <span class="el-icon-star-on" />
+              <span>{{ post.likes.length }}</span>
+            </el-button>
+            <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
+              <span class="el-icon-star-off" />
+              <span>{{ post.likes.length }}</span>
+            </el-button>
+          </p>
+        </no-ssr>
         <p class="text-right">{{ post.created_at | time}}</p>
       </el-card>
       <p>
